eac604a375e745a96b991832c88aa1dd56f8ec3f
Hongsu Wang <sudospace@gmail.com>
2024-10-07 23:22:33 -0400

    update encoding url in copy url func

--- app/page.tsx
+++ app/page.tsx
@@ -69,11 +69,11 @@ export default function Home() {
     const task2 = sectionStates[1].selectedTask;
     const task3 = sectionStates[2].selectedTask;
     const input1 = sectionStates[0].inputValue;
-    let ext_url = encodeURIComponent(apiUrl);
-    ext_url = ext_url.replace(/&/g, "%26");
+    const ext_url = apiUrlRef.current;
     const base_url = window.location.href.split('?')[0];
     // const full_url = `${base_url}?task1=${task1}&task2=${task2}&task3=${task3}&input1=${input1}&ext_url=${ext_url}&auto_run=true`;
-    const full_url = `${base_url}?task1=${task1}&task2=${task2}&task3=${task3}&input1=${input1}&ext_url=${ext_url}`;
+    let full_url = `${base_url}?task1=${task1}&task2=${task2}&task3=${task3}&input1=${input1}&ext_url=${ext_url}`;
+    full_url = encodeURI(full_url);
     // Copy to clipboard
     await navigator.clipboard.writeText(full_url);
     const chatStatsElement = document.getElementById("status");
